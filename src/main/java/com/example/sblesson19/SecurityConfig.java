1fd7adca6894024057f2ec105ae7fde5bf4e174b
Dave Wolf <dave45678@gmail.com>
2018-08-23 17:48:41 -0400

    cleaned up unused imports

--- src/main/java/com/example/sblesson19/SecurityConfig.java
+++ src/main/java/com/example/sblesson19/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.example.sblesson19;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -17,13 +15,13 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public PasswordEncoder encoder() {
+    protected PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
 
     @Override
-    public void configure(HttpSecurity http) throws Exception
+    protected void configure(HttpSecurity http) throws Exception
     {
         //Restricts access to routes
         http.authorizeRequests()
@@ -39,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         }
 
     @Override
-    public void configure (AuthenticationManagerBuilder auth) throws Exception
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception
     {
         auth.inMemoryAuthentication()
                 .withUser("dave").password(encoder().encode("password")).authorities("ADMIN").
