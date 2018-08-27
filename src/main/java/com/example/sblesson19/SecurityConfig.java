8b4b6db9d945badd0c21c77ab080404d8614d12a
Dave Wolf <dave45678@gmail.com>
2018-08-27 14:46:57 -0400

    removed import to old password encoder

--- src/main/java/com/example/sblesson19/SecurityConfig.java
+++ src/main/java/com/example/sblesson19/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -15,7 +14,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    protected PasswordEncoder encoder() {
+    protected BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
 
